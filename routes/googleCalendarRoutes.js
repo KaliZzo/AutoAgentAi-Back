fdd5adf5a3db38ca514a8bedc43b0d39aae50222
nisim <nisimohana44@gmail.com>
2024-11-07 10:53:04 +0200

    Controller Google map update

--- routes/googleCalendarRoutes.js
+++ routes/googleCalendarRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const googleCalendarController = require("../controllers/googleCalenderController")
+const jwtMiddleware = require("./../middleware/jwtHandler") // Make sure you have JWT to acces to the page you want.
 
-router.get("/auth", googleCalendarController.getAuthURL) //Step 1 get access to google account
-router.get("/auth/callback", googleCalendarController.getTokens) // Step 2 Get Token Access
-router.post("/add-event", googleCalendarController.addEvent) // Step 3 Add the Event
+router.get("/auth", jwtMiddleware, googleCalendarController.getAuthURL) //Step 1 get access to google account
+router.get("/auth/callback", jwtMiddleware, googleCalendarController.getTokens) // Step 2 Get Token Access
+router.post("/add-event", jwtMiddleware, googleCalendarController.addEvent) // Step 3 Add the Event
 
 module.exports = router
